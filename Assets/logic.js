04e7eb189c9d2e97b7da37c892037527cbed9f24
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-01-07 21:35:13 -0600

    update

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,60 +1,81 @@
-// const highScoreButton = document.getElementById("#highScoreButton")
-// var countDownTimer = document.querySelector("#countDownTimer")
-
-// var secondsLeft = 10;
-
-// timer =60s;
-// if timer ===0 game over
-// else time left -penalty
-
-// var questions = [
-//     {
-//         title: "question",
-//         choices: ["", ""],
-//         answer: ""
-//     }
-// ]
-
-
-
-// function setTime() {
-//     var timerInterval = setInterval(function() {
-//       secondsLeft--;
-//       countDownTimer.textContent = secondsLeft + " Seconds Remaining" ;
-  
-//       if(secondsLeft === 0) {
-//         clearInterval(timerInterval);
-//         sendMessage();
-//       }
-  
-//     }, 1000);
-//   }
-
-// function sendMessage() {
-//     countDownTimer.textContent = " ";
-//     var FAILURE = document.createElement("FAILURE");
-//     FAILURE.setAttribute("src","Failure");
-//     countDownTimer.appendChild(FAILURE);
-  
-// }
-  
-// setTime();
+const questions =[
+  {
+    question: "testing",
+    answers: [
+      {text: "test", correct: false},
+      {text: "test", correct: true},
+      {text: "test", correct: false},
+      {text: "test", correct: false}
+    ]
+  },
+  {
+    question: "testing",
+    answers: [
+      {text: "test", correct: false},
+      {text: "test", correct: true},
+      {text: "test", correct: false},
+      {text: "test", correct: false}
+    ]
+  },
+  {
+    question: "testing",
+    answers: [
+      {text: "test", correct: false},
+      {text: "test", correct: true},
+      {text: "test", correct: false},
+      {text: "test", correct: false}
+    ]
+  },
+  {
+    question: "testing",
+    answers: [
+      {text: "test", correct: false},
+      {text: "test", correct: true},
+      {text: "test", correct: false},
+      {text: "test", correct: false}
+    ]
+  }
+]
 
 const startButton = document.getElementById("startbtn");
 const nextButton =document.getElementById("nextbtn")
 const questionContainerElement = document.getElementById("questionContainer");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answerbtn");
+var timeEl = document.querySelector("#timer");
+var secondsLeft = 5;
+var count = localStorage.getItem("count");
+
+count = secondsLeft;
 
 let shuffledQuestions, currentQuesitonIndex
 
 startButton.addEventListener("click", startGame)
 nextButton.addEventListener("click", () =>{
   currentQuesitonIndex++
-  setNextQuestion()
+  setNextQuestion();
+  // localStorage.setItem("count", count)
 })
 
+function setTime(){
+  var timerInterval = setInterval(function(){
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left";
+
+    if(secondsLeft ===0){
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+
+function sendMessage(){
+  timeEl.textContent = "Times UP! ";
+
+}
+
 function startGame(){
+  setTime();
   startButton.classList.add("hide")
   shuffledQuestions = questions.sort(() => Math.random() -.5)
   currentQuesitonIndex = 0
@@ -103,6 +124,8 @@ function selectAnswer(e){
   } else {
     startButton.innerText = "Restart"
     startButton.classList.remove("hide")
+    nextButton.classList.remove("hide")
+    nextButton.innerText = "Submit"
   }
 }
 
@@ -120,41 +143,41 @@ function clearStatusClass(element){
   element.classList.remove("wrong")
 }
 
-const questions =[
-  {
-    question: "testing",
-    answers: [
-      {text: "test", correct: false},
-      {text: "test", correct: true},
-      {text: "test", correct: false},
-      {text: "test", correct: false}
-    ]
-  },
-  {
-    question: "testing",
-    answers: [
-      {text: "test", correct: false},
-      {text: "test", correct: true},
-      {text: "test", correct: false},
-      {text: "test", correct: false}
-    ]
-  },
-  {
-    question: "testing",
-    answers: [
-      {text: "test", correct: false},
-      {text: "test", correct: true},
-      {text: "test", correct: false},
-      {text: "test", correct: false}
-    ]
-  },
-  {
-    question: "testing",
-    answers: [
-      {text: "test", correct: false},
-      {text: "test", correct: true},
-      {text: "test", correct: false},
-      {text: "test", correct: false}
-    ]
-  }
-]
+// const questions =[
+//   {
+//     question: "testing",
+//     answers: [
+//       {text: "test", correct: false},
+//       {text: "test", correct: true},
+//       {text: "test", correct: false},
+//       {text: "test", correct: false}
+//     ]
+//   },
+//   {
+//     question: "testing",
+//     answers: [
+//       {text: "test", correct: false},
+//       {text: "test", correct: true},
+//       {text: "test", correct: false},
+//       {text: "test", correct: false}
+//     ]
+//   },
+//   {
+//     question: "testing",
+//     answers: [
+//       {text: "test", correct: false},
+//       {text: "test", correct: true},
+//       {text: "test", correct: false},
+//       {text: "test", correct: false}
+//     ]
+//   },
+//   {
+//     question: "testing",
+//     answers: [
+//       {text: "test", correct: false},
+//       {text: "test", correct: true},
+//       {text: "test", correct: false},
+//       {text: "test", correct: false}
+//     ]
+//   }
+// ]
